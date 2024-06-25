a11774ba5ee74adf10068ef1c0cb9b7ae1f82cfe
umilojkovic <uroshm@gmail.com>
2024-06-25 19:44:43 -0400

    add backend express setup, Dockerfile, update gitignore.

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
+  
